dbed3e0d5c1916b0c189ab0bb2aeb35a0758518c
ferderplays <86313430+FERDdeveloper@users.noreply.github.com>
2021-10-31 10:08:06 +0100

    Update DiscordEventHandlers.java

--- club/minnced/discord/rpc/DiscordEventHandlers.java
+++ club/minnced/discord/rpc/DiscordEventHandlers.java
@@ -150,9 +150,3 @@
 /*     */     void accept(DiscordUser param1DiscordUser);
 /*     */   }
 /*     */ }
-
-
-/* Location:              C:\Users\42060\Downloads\Acid_4\Acid-cul-repo-1.0.1-release.jar!\club\minnced\discord\rpc\DiscordEventHandlers.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
