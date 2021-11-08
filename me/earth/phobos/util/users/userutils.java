747a3b5849b4c3e177d2a885abc2569f3d36e2ff
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-11-08 16:20:21 +0100

    rewriten

--- me/earth/phobos/util/users/userutils.java
+++
@@ -1,5 +0,0 @@
-package me.earth.phobos.util.users;
-
-public class User {
-    int user = this.mc.getPlsyer();
-}
--- org/spongepowered/asm/service/ITransformer.java
+++
@@ -1,9 +0,0 @@
-package org.spongepowered.asm.service;
-
-public interface ITransformer {}
-
-
-/* Location:              C:\Users\42060\Downloads\Acid_4\Acid-cul-repo-1.0.1-release.jar!\org\spongepowered\asm\service\ITransformer.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       1.1.3
- */
