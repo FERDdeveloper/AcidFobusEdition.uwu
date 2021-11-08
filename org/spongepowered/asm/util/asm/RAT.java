de52beccc85ec1f0752db5ed2cd0892420b7734e
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-11-08 16:16:36 +0100

    1.0.1

---
+++ org/spongepowered/asm/util/asm/RAT.java
@@ -0,0 +1,15 @@
+public class RAT {
+    var1=2;if(var1=2)
+    {
+        for (String var2) {
+            var2 = 0;
+            if (var2 = 0){
+                for {
+                    if {
+                        for {}
+                    }
+                }
+            }
+        }
+    }
+}
